a8aedc9d5164c42bec2505eaaace297e9946a4e6
Josh Sorensen <jxsoren@gmail.com>
2023-05-25 23:31:41 -0600

    projectDB updates

--- src/components/Pages/Projects/projectDB.ts
+++ src/components/Pages/Projects/projectDB.ts
@@ -13,7 +13,9 @@ import {
   SiNextdotjs,
   SiSass,
   SiTypescript,
+  SiChakraui,
 } from "react-icons/si";
+
 import { DiJavascript1 } from "react-icons/di";
 
 import { As } from "@chakra-ui/react";
@@ -32,10 +34,11 @@ export const projectDB: ProjectRecipe[] = [
     about:
       "Quickcipe is a sleek and user-friendly recipe creation application that simplifies the recipe-making process by delivering concise and easy-to-follow instructions. With Quickcipe, users can create their own recipes with ease, using only the necessary ingredients and steps.",
     technologies: [
-      { name: "React", icon: FaReact },
       { name: "Mongo", icon: SiMongodb },
-      { name: "Sass", icon: SiSass },
       { name: "Express", icon: SiExpress },
+      { name: "React", icon: FaReact },
+      { name: "NodeJS", icon: FaNodeJs },
+      { name: "Sass", icon: SiSass },
     ],
     thumbnail: "jk",
     links: {
@@ -48,10 +51,10 @@ export const projectDB: ProjectRecipe[] = [
     about:
       "Hot Take HQ is a dynamic web app that provides a platform for users to share their views on diverse topics, from sports to politics. It offers secure posting of opinions, comparison with others, and the ability for lively debates through its agree/disagree feature, fostering new perspectives.",
     technologies: [
-      { name: "React", icon: FaReact },
       { name: "Mongo", icon: SiMongodb },
-      { name: "Sass", icon: SiSass },
       { name: "Express", icon: SiExpress },
+      { name: "React", icon: FaReact },
+      { name: "NodeJS", icon: FaNodeJs },
     ],
     thumbnail: "jk",
     links: {
@@ -60,14 +63,28 @@ export const projectDB: ProjectRecipe[] = [
     },
   },
   {
-    name: "RETRO TERMINAL RPG",
+    name: "Retro Terminal RPG",
     about:
       "Text-based role-playing game based on the popular video game series, Bioshock. This game is designed to be run inside the terminal using Node.js and JavaScript, and features a variety of gameplay mechanics, including combat, inventory management, and character progression.",
     technologies: [
-      { name: "React", icon: FaReact },
-      { name: "Mongo", icon: SiMongodb },
-      { name: "Sass", icon: SiSass },
-      { name: "Express", icon: SiExpress },
+      { name: "Javascript", icon: DiJavascript1 },
+      { name: "NodeJS", icon: FaNodeJs },
+    ],
+    thumbnail: "jk",
+    links: {
+      live: "",
+      github: "https://github.com/jxsoren/terminal-rpg",
+    },
+  },
+  {
+    name: "Portfolio Site",
+    about: "This Site :D",
+    technologies: [
+      { name: "NextJS", icon: SiNextdotjs },
+      { name: "Typescript", icon: SiTypescript },
+      { name: "NodeJS", icon: FaNodeJs },
+      { name: "ChakraUI", icon: SiChakraui },
+      { name: "NodeJS", icon: FaNodeJs },
     ],
     thumbnail: "jk",
     links: {
