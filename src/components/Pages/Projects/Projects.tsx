ff80694d869b06a85c280b6ed5ad3fc8e75305c5
Josh Sorensen <jxsoren@gmail.com>
2023-06-17 15:10:27 -0600

    margin fixes

--- src/components/Pages/Projects/Projects.tsx
+++ src/components/Pages/Projects/Projects.tsx
@@ -18,30 +18,39 @@ const Projects = () => {
   const { textColor } = useAppColors();
 
   return (
-    <Container maxW="2x1" color={textColor} centerContent>
-      <Heading marginBottom={4}>Projects</Heading>
-      <Flex
-        direction={["column", "row"]}
-        align="center"
-        justify="center"
-        padding={4}
-        maxW={{ base: "100%", md: "100%" }}
-        zIndex={3}
-      >
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing="50px">
-          {projectDB.map((project) => (
-            <ProjectCard
-              key={project.name}
-              name={project.name}
-              about={project.about}
-              technologies={project.technologies}
-              thumbnail={project.thumbnail}
-              links={project.links}
-            />
-          ))}
-        </SimpleGrid>
-      </Flex>
-    </Container>
+    <Box
+      aspectRatio={960 / 540}
+      width="100%"
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      backgroundColor="#183664"
+    >
+      <Container maxW="2x1" color={textColor} centerContent>
+        <Heading marginBottom={4}>Projects</Heading>
+        <Flex
+          direction={["column", "row"]}
+          align="center"
+          justify="center"
+          padding={4}
+          maxW={{ base: "100%", md: "100%" }}
+          zIndex={3}
+        >
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing="50px">
+            {projectDB.map((project) => (
+              <ProjectCard
+                key={project.name}
+                name={project.name}
+                about={project.about}
+                technologies={project.technologies}
+                thumbnail={project.thumbnail}
+                links={project.links}
+              />
+            ))}
+          </SimpleGrid>
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 
