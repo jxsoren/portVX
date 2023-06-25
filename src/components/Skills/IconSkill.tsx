67ab86847a49b3d2167a7f1c9ad8174f30586031
Josh Sorensen <jxsoren@gmail.com>
2023-06-25 02:05:33 -0600

    "size change"

--- src/components/Skills/IconSkill.tsx
+++ src/components/Skills/IconSkill.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Text, ListItem, Icon, Box } from "@chakra-ui/react";
+import { Text, ListItem, Icon, Box, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 
 import { TechSkill } from "./skillDB";
@@ -9,7 +9,7 @@ const IconSkill: React.FC<TechSkill> = ({ name, icon, color }) => {
   return (
     <ListItem m={4}>
       <Box textAlign="center">
-        <Icon as={icon} w={40} h={40} color={color} />
+        <Icon as={icon} w={12} h={12} color={color} />
         <Text color="#ffffff">{name}</Text>
       </Box>
     </ListItem>
