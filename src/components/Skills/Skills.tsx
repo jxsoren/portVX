032567b0233ea915108c52093ad4e9b807bd2633
Josh Sorensen <jxsoren@gmail.com>
2023-06-25 23:55:20 -0600

    "grid layout change"

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -7,15 +7,27 @@ import {
   Text,
   ListItem,
   Box,
+  SimpleGrid,
   OrderedList,
+  Grid,
+  GridItem,
+  HStack,
+  useColorModeValue,
+  List,
+  VStack,
 } from "@chakra-ui/react";
 
 import { useAppColors } from "@/hooks/useAppColors";
 import IconSkill from "./IconSkill";
 import { skillsDB } from "./skillDB";
 
+import SkillCategory from "./SkillCatagory";
+
 const Skills = () => {
   const { textColor } = useAppColors();
+
+  const { languages, frontEnd, frameworks, backEnd, platforms, CCID, cloud } =
+    skillsDB;
 
   return (
     <Box
@@ -27,31 +39,61 @@ const Skills = () => {
       background="linear-gradient(#16161d, #1f1f3a, #3b2f4a)"
     >
       <Container maxW="2x1" centerContent>
-        <Flex
-          direction="column"
-          align="center"
-          justify="center"
+        <Heading color="#ffffff" fontSize="6xl" mb={20}>
+          Skills
+        </Heading>
+
+        <Grid
+          templateRows="repeat(2, 2fr)"
+          templateColumns="repeat(2, 2fr)"
+          gap={4}
           padding={4}
           maxW={{ base: "90%", md: "70%" }}
+          position="relative"
+          gridRowGap={1} 
         >
-          <Heading color="#ffffff">Skills</Heading>
-          <Flex
-            as={OrderedList}
-            direction="row"
-            justifyContent="space-between"
-            listStyleType="none"
-            flexWrap="wrap"
-          >
-            {skillsDB.map((skill) => (
-              <IconSkill
-                key={skill.name}
-                name={skill.name}
-                icon={skill.icon}
-                color={skill.color}
-              />
-            ))}
-          </Flex>
-        </Flex>
+          <GridItem rowSpan={1} colSpan={1} rowStart={1} colStart={2}>
+            <Box
+              mb={4}
+              shadow="base"
+              borderWidth="2px"
+              borderColor={useColorModeValue("gray.200", "gray.500")}
+              borderRadius={"x2"}
+              background="linear-gradient(#6600FF, #00CC99, #6600FF)"
+              position="relative"
+              width={{ base: "100%", md: "100%" }}
+              top={0}
+              left={0}
+            >
+              <List spacing={2} textAlign="start">
+                <SkillCategory title="Languages" skills={languages} />
+                <SkillCategory title="Frameworks" skills={frameworks} />
+                <SkillCategory title="Front-end" skills={frontEnd} />
+                <SkillCategory title="Back-end" skills={backEnd} />
+              </List>
+            </Box>
+          </GridItem>
+
+          <GridItem rowSpan={1} colSpan={1} rowStart={2} colStart={1}>
+            <Box
+              mb={4}
+              shadow="base"
+              borderWidth="2px"
+              borderColor={useColorModeValue("gray.200", "gray.500")}
+              borderRadius={"x2"}
+              background="linear-gradient(#00CC99, #6600FF, #00CC99)"
+              position="relative"
+              top={0}
+              left={0}
+            >
+              <List spacing={3} textAlign="start">
+                <SkillCategory title="Platforms" skills={platforms} />
+                <SkillCategory title="Cloud" skills={cloud} />
+                <SkillCategory title="CCID" skills={CCID} />
+              </List>
+            </Box>
+          </GridItem>
+        </Grid>
       </Container>
     </Box>
   );
