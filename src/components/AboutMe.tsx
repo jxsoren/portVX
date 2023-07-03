c1f661b87259f8b0e05e10e3e6bc75b99b005aa3
Josh Sorensen <jxsoren@gmail.com>
2023-07-02 19:25:41 -0600

    "animaiton on scroll"

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -10,8 +10,15 @@ import {
   useTheme,
 } from "@chakra-ui/react";
 
+import { motion } from "framer-motion";
+import { useScrollAnimation, getAnimations } from "@/hooks/useScroll";
+const MotionBox = motion(Box);
+
 const AboutMe = () => {
   const theme = useTheme();
+
+  const [ref, inView] = useScrollAnimation();
+  const { rightToLeft, leftToRight } = getAnimations();
 
   return (
     <Box
@@ -35,25 +42,43 @@ const AboutMe = () => {
           maxW={{ base: "90%", md: "70%" }}
           fontFamily={theme.fonts.body}
         >
-          <Text
-            fontSize={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
-            mt={10}
-            fontFamily={theme.fonts.body}
+          <MotionBox
+            ref={ref}
+            variants={leftToRight}
+            animate={inView ? "show" : "hidden"}
           >
-            {`Greetings! My name is Josh Sorensen, a determined problem solver based out of Salt Lake City, Utah.`}
-          </Text>
-          <Text
-            fontSize={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
-            mt={10}
+            <Text
+              fontSize={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
+              mt={10}
+              fontFamily={theme.fonts.body}
+            >
+              {`Greetings! My name is Josh Sorensen, a determined problem solver based out of Salt Lake City, Utah.`}
+            </Text>
+          </MotionBox>
+          <MotionBox
+            ref={ref}
+            variants={rightToLeft}
+            animate={inView ? "show" : "hidden"}
           >
-            {`Intrigued by the world of coding and technology, I've dedicated my career to creating efficient and user-friendly solutions. I have a strong knack for problem-solving and enjoy the challenge of breaking down complex issues into manageable tasks. From meticulously crafting user interfaces to structuring robust backend architectures, my goal is to continuously grow, learn, and push the boundaries of what I can achieve.`}
-          </Text>
-          <Text
-            fontSize={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
-            mt={10}
+            <Text
+              fontSize={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
+              mt={10}
+            >
+              {`Intrigued by the world of coding and technology, I've dedicated my career to creating efficient and user-friendly solutions. I have a strong knack for problem-solving and enjoy the challenge of breaking down complex issues into manageable tasks. From meticulously crafting user interfaces to structuring robust backend architectures, my goal is to continuously grow, learn, and push the boundaries of what I can achieve.`}
+            </Text>
+          </MotionBox>
+          <MotionBox
+            ref={ref}
+            variants={leftToRight}
+            animate={inView ? "show" : "hidden"}
           >
-            {`Apart from developing software, my passions lie in exploring Utah's breathtaking landscapes, delving into literature and philosophy, and maintaining an active routine at the gym.`}
-          </Text>
+            <Text
+              fontSize={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
+              mt={10}
+            >
+              {`Apart from developing software, my passions lie in exploring Utah's breathtaking landscapes, delving into literature and philosophy, and maintaining an active routine at the gym.`}
+            </Text>
+          </MotionBox>
         </Flex>
       </Container>
     </Box>
