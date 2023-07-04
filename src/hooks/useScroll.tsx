5df94f70e03551b65be78b6993f056fea7b9ae3d
Josh Sorensen <jxsoren@gmail.com>
2023-07-03 22:00:40 -0600

    "added up animation"

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -56,6 +56,16 @@ const animations = {
         },
       },
     },
+    up: {
+      hidden: { opacity: 0, y: +100 },
+      show: {
+        opacity: 1,
+        x: 0,
+        transition: {
+          duration: 1,
+        },
+      },
+    },
   };
 
 export const getAnimations = () => animations;
