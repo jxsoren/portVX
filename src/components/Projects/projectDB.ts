861b3205532e765826c524ff4b540d0b14e601ec
Josh Sorensen <jxsoren@gmail.com>
2023-07-04 19:02:42 -0600

    "added color"

--- src/components/Projects/projectDB.ts
+++ src/components/Projects/projectDB.ts
@@ -15,10 +15,12 @@ import { CgWebsite } from "react-icons/cg";
 
 import { As } from "@chakra-ui/react";
 
+import { TechSkill } from "../Skills/skillDB";
+
 export interface ProjectRecipe {
   name: string;
   about: string;
-  technologies: { name: string; icon: As }[];
+  technologies: TechSkill[];
   thumbnail: string;
   links: {
     name: string;
@@ -33,12 +35,12 @@ export const projectDB: ProjectRecipe[] = [
     about:
       "Quickcipe is a sleek and user-friendly recipe creation application that simplifies the recipe-making process by delivering concise and easy-to-follow instructions. With Quickcipe, users can create their own recipes with ease, using only the necessary ingredients and steps.",
     technologies: [
-      { name: "Javascript", icon: DiJavascript1 },
-      { name: "Mongo", icon: SiMongodb },
-      { name: "Express", icon: SiExpress },
-      { name: "React", icon: FaReact },
-      { name: "NodeJS", icon: FaNodeJs },
-      { name: "Sass", icon: SiSass },
+      { name: "Javascript", icon: DiJavascript1, color: "#f7df1e" },
+      { name: "Mongo", icon: SiMongodb, color: "#4db33d" },
+      { name: "Express", icon: SiExpress, color: "#000000" },
+      { name: "React", icon: FaReact, color: "#61dafb" },
+      { name: "NodeJS", icon: FaNodeJs, color: "#68a063" },
+      { name: "Sass", icon: SiSass, color: "#c69" },
     ],
     thumbnail: "./thumbnails/quickcipie.png",
     links: [
@@ -59,11 +61,11 @@ export const projectDB: ProjectRecipe[] = [
     about:
       "Hot Take HQ is a versatile web app where users can securely post and compare opinions on topics ranging from sports to politics, engaging in vibrant debates via its agree/disagree feature to cultivate fresh perspectives.",
     technologies: [
-      { name: "Javascript", icon: DiJavascript1 },
-      { name: "Mongo", icon: SiMongodb },
-      { name: "Express", icon: SiExpress },
-      { name: "React", icon: FaReact },
-      { name: "NodeJS", icon: FaNodeJs },
+      { name: "Javascript", icon: DiJavascript1, color: "#f7df1e" },
+      { name: "Mongo", icon: SiMongodb, color: "#4db33d" },
+      { name: "Express", icon: SiExpress, color: "#000000" },
+      { name: "React", icon: FaReact, color: "#61dafb" },
+      { name: "NodeJS", icon: FaNodeJs, color: "#68a063" },
     ],
     thumbnail: "./thumbnails/hotTakeHQ.png",
     links: [
@@ -84,8 +86,8 @@ export const projectDB: ProjectRecipe[] = [
     about:
       "Text-based role-playing game based on the popular video game series, Bioshock. This game is designed to be run inside the terminal using Node.js and JavaScript, and features a variety of gameplay mechanics, including combat, inventory management, and character progression.",
     technologies: [
-      { name: "Javascript", icon: DiJavascript1 },
-      { name: "NodeJS", icon: FaNodeJs },
+      { name: "Javascript", icon: DiJavascript1, color: "#f7df1e" },
+      { name: "NodeJS", icon: FaNodeJs, color: "#68a063" },
     ],
     thumbnail: "./thumbnails/nodeRPG.png",
     links: [
@@ -106,11 +108,11 @@ export const projectDB: ProjectRecipe[] = [
     about:
       "A testament to innovation and continuous learning, this project uses Chakra UI for user interaction, TypeScript for reliable coding, and Next.js for optimal performance. It reflects my journey in tech and commitment to expanding my capabilities.",
     technologies: [
-      { name: "NextJS", icon: SiNextdotjs },
-      { name: "Typescript", icon: SiTypescript },
-      { name: "NodeJS", icon: FaNodeJs },
-      { name: "ChakraUI", icon: SiChakraui },
-      { name: "NodeJS", icon: FaNodeJs },
+      { name: "NextJS", icon: SiNextdotjs, color: "#000000" },
+      { name: "Typescript", icon: SiTypescript, color: "#007acc" },
+      { name: "NodeJS", icon: FaNodeJs, color: "#68a063" },
+      { name: "ChakraUI", icon: SiChakraui, color: "#319795" },
+      { name: "NodeJS", icon: FaNodeJs, color: "#68a063" },
     ],
     thumbnail: "./thumbnails/portfolioVX.png",
     links: [
