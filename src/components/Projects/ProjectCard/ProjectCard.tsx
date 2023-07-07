f0b66af2a8d483aaeb2bb6578ea001c9f73b044e
Josh Sorensen <jxsoren@gmail.com>
2023-07-06 22:26:23 -0600

    "animation testing"

--- src/components/Projects/ProjectCard/ProjectCard.tsx
+++ src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -21,6 +21,8 @@ import ProjectTechnologies from "./CardComponents/ProjectTechnologies";
 import ProjectAbout from "./CardComponents/ProjectAbout";
 import ProjectLinks from "./CardComponents/ProjectLinks";
 
+import { AnimatedBox, getAnimations } from "@/hooks/useScroll";
+
 const ProjectCard: React.FC<ProjectRecipe> = ({
   name,
   about,
@@ -29,34 +31,49 @@ const ProjectCard: React.FC<ProjectRecipe> = ({
   links,
 }) => {
   const { textColor } = useAppColors();
+  const {
+    rightToLeft,
+    leftToRight,
+    up,
+    down,
+    flip,
+    slideFade,
+    skew,
+    wobble,
+    bounce,
+    slideBlur
+  } = getAnimations();
+
   return (
-    <Container>
-      <Card
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        variant="outline"
-        boxShadow={"lg"}
-        height={"100%"}
-        maxHeight={"800px"}
-        borderColor={textColor}
-        color={textColor}
-      >
-        <CardHeader textAlign="center" width="100%">
-          <Heading size="lg">{name}</Heading>
-        </CardHeader>
-        <CardBody overflowY="auto">
-          <Flex direction="column" justify="space-between" height="100%">
-            <Stack divider={<StackDivider />} spacing="4">
-              <ProjectThumbnail thumbnail={thumbnail} />
-              <ProjectAbout about={about} />
-              <ProjectTechnologies technologies={technologies} />
-              <ProjectLinks links={links} />
-            </Stack>
-          </Flex>
-        </CardBody>
-      </Card>
-    </Container>
+    <AnimatedBox animation={slideFade}>
+      <Container>
+        <Card
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          variant="outline"
+          boxShadow={"lg"}
+          height={"100%"}
+          maxHeight={"800px"}
+          borderColor={textColor}
+          color={textColor}
+        >
+          <CardHeader textAlign="center" width="100%">
+            <Heading size="lg">{name}</Heading>
+          </CardHeader>
+          <CardBody overflowY="auto">
+            <Flex direction="column" justify="space-between" height="100%">
+              <Stack divider={<StackDivider />} spacing="4">
+                <ProjectThumbnail thumbnail={thumbnail} />
+                <ProjectAbout about={about} />
+                <ProjectTechnologies technologies={technologies} />
+                <ProjectLinks links={links} />
+              </Stack>
+            </Flex>
+          </CardBody>
+        </Card>
+      </Container>
+    </AnimatedBox>
   );
 };
 
