0355d9a802d0db7e840fd3749a98deba8c5fe13d
Josh Sorensen <jxsoren@gmail.com>
2023-07-19 23:37:49 -0600

    "removed unused theme elements"

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -22,18 +22,6 @@ const theme = extendTheme({
       text: "#ffffff",
       background: "#000000",
     },
-    gray: {
-      50: "#F7FAFC",
-      100: "#EDF2F7",
-      200: "#E2E8F0",
-      300: "#CBD5E0",
-      400: "#A0AEC0",
-      500: "#718096",
-      600: "#4A5568",
-      700: "#2D3748",
-      800: "#1A202C",
-      900: "#171923",
-    },
   },
   fonts: {
     heading: `'Space Mono', sans-serif`,
