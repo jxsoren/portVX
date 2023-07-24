825dc894531f0d35f83273ca15a7b3c9e510f0bf
Josh Sorensen <jxsoren@gmail.com>
2023-07-23 22:56:35 -0600

    "responsive change"

--- src/components/Skills/IconSkill.tsx
+++ src/components/Skills/IconSkill.tsx
@@ -1,15 +1,41 @@
-"use client";
-
-import { Text, ListItem, Icon, Box, SimpleGrid } from "@chakra-ui/react";
+import {
+  Text,
+  ListItem,
+  Icon,
+  Box,
+  SimpleGrid,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import React from "react";
 
 import { TechSkill } from "./skillDB";
 
 const IconSkill: React.FC<TechSkill> = ({ name, icon, color }) => {
+  const iconHeight = useBreakpointValue({
+    xs: "60px",
+    sm: "50px",
+    md: "50px",
+    lg: "50px",
+  });
+
+  const iconWidth = useBreakpointValue({
+    xs: "60px",
+    sm: "50px",
+    md: "50px",
+    lg: "60px",
+  });
+
+  const fontSize = useBreakpointValue({
+    xs: "10px",
+    sm: "16px",
+    md: "14px",
+    lg: "12px",
+  });
+
   return (
     <Box textAlign="center">
-      <Icon as={icon} w={100} h={20} color={color} />
-      <Text fontSize="small" color={color}>
+      <Icon as={icon} w={iconWidth} h={iconHeight} color={color} />
+      <Text fontSize={fontSize} color={color} fontWeight="bold">
         {name}
       </Text>
     </Box>
