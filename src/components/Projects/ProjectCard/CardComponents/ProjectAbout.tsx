2da3efbd57de24bca5f16c43fe1d402a72f9b5da
Josh Sorensen <jxsoren@gmail.com>
2023-07-27 22:25:46 -0600

    "added shorter about for smaller screens"

--- src/components/Projects/ProjectCard/CardComponents/ProjectAbout.tsx
+++ src/components/Projects/ProjectCard/CardComponents/ProjectAbout.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 
 interface ProjectAboutProps {
   about: string;
+  shortAbout: string;
 }
-const ProjectAbout: React.FC<ProjectAboutProps> = ({ about }) => {
+const ProjectAbout: React.FC<ProjectAboutProps> = ({ about, shortAbout }) => {
+  const isSmallScreen = useBreakpointValue({ base: true, sm: false });
   return (
     <Flex
       flex="1"
@@ -14,12 +16,13 @@ const ProjectAbout: React.FC<ProjectAboutProps> = ({ about }) => {
       alignItems="center"
       justifyContent="center"
       p={4}
+      minH={["10vh", "15vh", "26vh", "20vh"]}
     >
       <Heading size="xs" textTransform="uppercase" textAlign="center">
         About
       </Heading>
-      <Text pt="2" fontSize="17">
-        {about}
+      <Text pt="2" fontSize={["16"]}>
+        {isSmallScreen ? shortAbout : about}
       </Text>
     </Flex>
   );
