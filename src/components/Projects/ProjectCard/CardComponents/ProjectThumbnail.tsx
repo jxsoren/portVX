e7ba4f575b7ece7de1d248088b694066fc916e6e
Josh Sorensen <jxsoren@gmail.com>
2023-07-31 21:10:53 -0600

    "added breakpoints"

--- src/components/Projects/ProjectCard/CardComponents/ProjectThumbnail.tsx
+++ src/components/Projects/ProjectCard/CardComponents/ProjectThumbnail.tsx
@@ -1,6 +1,6 @@
 "use client;";
 
-import { Image, Container } from "@chakra-ui/react";
+import { Image, Container, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 
 interface ThumbnailProps {
@@ -8,12 +8,33 @@ interface ThumbnailProps {
 }
 
 const ProjectThumbnail: React.FC<ThumbnailProps> = ({ thumbnail }) => {
+  const width = useBreakpointValue({
+    base: "100%",
+    xs: "100%",
+    sm: "100%",
+    md: "100%",
+    lg: "100%",
+    xl: "100%",
+    xxl: "100%",
+  });
+
+  const height = useBreakpointValue({
+    base: "250px",
+    xs: "250px",
+    sm: "250px",
+    md: "250px",
+    lg: "250px",
+    xl: "250px",
+    xxl: "300px",
+  });
+
   return (
     <Container display="flex" justifyContent="center">
       <Image
-        width={600}
-        height={250}
+        width={width}
+        height={height}
         objectFit="cover"
+        objectPosition="center"
         borderRadius={20}
         src={thumbnail}
         alt="Project Thumbnail"
