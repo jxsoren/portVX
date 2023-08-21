9737c51ec00aa8a6054b3debc1d59301ad06f961
Josh Sorensen <jxsoren@gmail.com>
2023-08-20 22:16:11 -0600

    adjusted aspect ratio for mobile devices

--- src/components/LayeredSVG.tsx
+++ src/components/LayeredSVG.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { Box } from "@chakra-ui/react";
+import { Box, useBreakpointValue } from "@chakra-ui/react";
 
 const LayeredSVG = ({ bgI }: { bgI: string }) => {
+
+  const aspectRatio = useBreakpointValue({
+    base: 960 / 540,
+    sm: 960 / 1200,
+    xs: 960 / 1200,
+  });
+
   return (
     <Box
-      aspectRatio={960 / 540}
+      aspectRatio={aspectRatio}
       width="100%"
       bgPosition="center"
       bgRepeat="no-repeat"
