3a5e345761419d8bcbb0e635d768b8f967af584f
Josh Sorensen <jxsoren@gmail.com>
2023-08-22 20:34:19 -0600

    new meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,18 @@ export default function RootLayout({
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Josh Sorensen" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-startup-image"
+          href="/icons/apple-touch-startup-image.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body style={{ overflowX: "hidden" }}>
         <Providers>{children}</Providers>
