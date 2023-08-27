f8271f52337492e574ed926edd5ece4f7944bca2
Josh Sorensen <jxsoren@gmail.com>
2023-08-26 22:51:39 -0600

    added resume seciton to nav

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -44,13 +44,12 @@ const NavLink = ({ to, children }: { to: string; children: ReactNode }) => {
 };
 
 export default function Nav() {
-  const Links = ["Skills", "Projects", "About"];
+  const Links = ["Skills", "Projects", "Resume", "About"];
 
   return (
     <Box
       bg="black"
       px={4}
-      boxShadow="dark-lg"
       textColor="white"
       position="sticky"
       top={0}
