496db7db9244076aedb52db7cc60d09d2d62d07e
Josh Sorensen <jxsoren@gmail.com>
2023-08-28 20:26:20 -0600

    font size changes

--- src/components/Resume/ResumeCard/CardComponents/ResumeResponsibilities.tsx
+++ src/components/Resume/ResumeCard/CardComponents/ResumeResponsibilities.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Box, UnorderedList, ListItem, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  UnorderedList,
+  ListItem,
+  Heading,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 import React from "react";
 
@@ -11,12 +17,21 @@ interface ResumeResponsibilites {
 const ResumeResponsibilites: React.FC<ResumeResponsibilites> = ({
   responsibilities,
 }) => {
+  const fontSize = useBreakpointValue({
+    xs: "10",
+    sm: "12",
+    md: "14",
+    lg: "14",
+    xl: "16",
+    xxl: "16",
+  });
+
   return (
     <Box>
-      <Heading as="h2" textAlign="center" mb={4}>
+      <Heading as="h2" textAlign="center" mb={4} fontSize={[10, 24, 30]}>
         Responsibilities
       </Heading>
-      <UnorderedList spacing={3} ml={5} fontSize="lg">
+      <UnorderedList spacing={3} ml={5} fontSize={fontSize}>
         {responsibilities.map((responsibility, index) => (
           <ListItem key={index}>{responsibility}</ListItem>
         ))}
