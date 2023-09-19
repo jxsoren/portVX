38dc7d0feb0ce65a52508752a77a4b13bc6480a9
Josh Sorensen <jxsoren@gmail.com>
2023-09-18 18:50:02 -0600

    height and font size adjustments

--- src/components/Resume/ResumeCard/CardComponents/ResumeResponsibilities.tsx
+++ src/components/Resume/ResumeCard/CardComponents/ResumeResponsibilities.tsx
@@ -18,17 +18,26 @@ const ResumeResponsibilites: React.FC<ResumeResponsibilites> = ({
   responsibilities,
 }) => {
   const fontSize = useBreakpointValue({
-    xs: "14",
-    sm: "14",
-    md: "14",
-    lg: "14",
-    xl: "16",
-    xxl: "16",
+    xs: "12",
+    sm: "12",
+    md: "12",
+    lg: "13",
+    xl: "15",
+    xxl: "15",
+  });
+
+  const sectionHeight = useBreakpointValue({
+    xs: "100%",
+    sm: "100%",
+    md: "100%",
+    lg: "100%",
+    xl: "100%",
+    xxl: "100%",
   });
 
   return (
-    <Box>
-      <Heading as="h2" textAlign="center" mb={4} fontSize={[10, 24, 30]}>
+    <Box height={sectionHeight}>
+      <Heading as="h2" textAlign="center" mb={4} fontSize={[10, 20, 26]}>
         Responsibilities
       </Heading>
       <UnorderedList spacing={3} ml={5} fontSize={fontSize}>
